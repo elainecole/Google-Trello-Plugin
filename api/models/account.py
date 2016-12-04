3fc5bd69c3c019e548c3f50c4e4ebadeeb9aee44
Daniel Borstelmann <dborstelmann@wustl.edu>
2016-12-04 16:04:52 -0600

    fix tokens

--- api/models/account.py
+++ api/models/account.py
@@ -7,4 +7,4 @@ class Account(models.Model):
     Holds meta information about an Account, not used to login.
     '''
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    trello_token = models.CharField(max_length=127, blank=False)
+    trello_token = models.CharField(max_length=127, null=True, default=None)
